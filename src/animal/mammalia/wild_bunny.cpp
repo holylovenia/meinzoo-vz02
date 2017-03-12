65de451d12d18e0cc9cd25aa95bd12611a4b80ef
Oktavianus Handika <handikaoktavianus29@gmail.com>
2017-03-12 21:15:07 +0700

    added wild_bunny and wild_colibri

---
+++ src/animal/mammalia/wild_bunny.cpp
@@ -0,0 +1,92 @@
+#include "wild_bunny.h"
+
+WildBunny::WildBunny(int _x, int _y, int _weight) : defID(0), defRatioMeat(0), defRatioPlant(90), defLimbCount(4), defSkinType("Hairy"), defEatMeat(false), defEatPlant(true) {
+	//wildbunny
+	ID = defID;
+	Location.setX(_x);
+	Location.setY(_y);
+	ratioMeat = defRatioMeat;
+	ratioPlant = defRatioPlant;
+	isLandAnimal = true;
+	isWaterAnimal = false;
+	isAirAnimal = false;
+	weight = _weight;
+	//herbivora
+	eatMeat = defEatMeat;
+	eatPlant = defEatPlant;
+	AnimalFood::AddTotalPlant(ratioPlant * weight / 100);
+	for (int id = defID; id <= 21; id++) {
+		addEnemy(id);
+	}
+}
+
+void WildBunny::Interact() {
+	std::cout << "The wild bunny is looking at you with murderous intent" << std::endl;
+}
+
+void WildBunny::Move(int movement) {
+	if (movement == 1) // Move up
+	{
+		Location.setY(Location.getY()-1);
+	}
+	else if (movement == 2) // Move right
+	{
+		Location.setX(Location.getX()+1);
+	}
+	else if (movement == 3) // Move down
+	{
+		Location.setY(Location.getY()+1);
+	}
+	else // Move left
+	{
+		Location.setX(Location.getX()-1);
+	}
+}
+
+int WildBunny::getReqMeat() {
+	return(ratioMeat * weight / 100);
+}
+
+int WildBunny::getReqPlant() {
+	return(ratioPlant * weight / 100);
+}
+
+bool WildBunny::IsLandAnimal() {
+	return(isLandAnimal);
+}
+
+bool WildBunny::IsWaterAnimal() {
+	return(isWaterAnimal);
+}
+
+bool WildBunny::IsAirAnimal() {
+	return(isAirAnimal);
+}
+
+bool WildBunny::isHerbivore() {
+	return (!eatMeat && eatPlant);
+}
+bool WildBunny::isCarnivore() {
+	return (eatMeat && !eatPlant);
+}
+bool WildBunny::isOmnivore() {
+	return (eatMeat && eatPlant);
+}
+
+void WildBunny::setBehavior() {
+	isWild = false;
+}
+
+// Setter
+void WildBunny::setLocation(int _x, int _y) {
+	Location.setX(_x);
+	Location.setY(_y);
+}
+
+// Getter
+int WildBunny::getX() const {
+	return Location.getX();
+}
+int WildBunny::getY() const {
+	return Location.getY();
+}
---
+++ src/animal/mammalia/wild_bunny.h
@@ -0,0 +1,71 @@
+#ifndef WILD_BUNNY_H
+#define WILD_BUNNY_H
+
+#include <set>
+#include <string>
+#include <iostream>
+#include "../animalfood.h"
+#include "../../misc/point.h"
+
+class WildBunny {
+	public :
+		WildBunny(int _x, int _y, int _weight);
+		//animal
+		void Interact();
+		void Move(int movement);
+		bool IsLandAnimal();
+		bool IsWaterAnimal();
+		bool IsAirAnimal();
+		//animaldiet
+		bool isHerbivore();
+		bool isCarnivore();
+		bool isOmnivore();
+
+		int getReqMeat();
+		int getReqPlant();
+
+		//behaviorwild
+		void setBehavior();
+		void addEnemy(int x);
+		void removeEnemy(int x);
+		bool isEnemy(int x);
+
+		//point setter
+		void setLocation(int _x, int _y);
+		//point getter
+		int getX() const;
+		int getY() const;
+		//behavior
+		bool getBehavior();
+	private:
+		std::set<int> enemy;
+		//point
+		Point Location;
+		//animal
+		int ID;
+		int limbCount;
+		std::string skinType;
+		bool isLandAnimal;
+		bool isWaterAnimal;
+		bool isAirAnimal;
+		//animaldiet
+		int weight;
+		bool eatMeat;
+		bool eatPlant;
+		int ratioMeat;
+		int ratioPlant;
+		//herbivora
+		const bool defEatMeat;
+		const bool defEatPlant;
+		//aves
+		const int defLimbCount;
+		const std::string defSkinType;
+		//colibri
+		const int defID;
+        const int defRatioMeat;
+        const int defRatioPlant;
+        //behavior
+        bool isWild;
+};
+
+#endif
